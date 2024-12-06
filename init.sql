5dbd6a8d86e3d5df7d99f4a793439fec5b1f7507
Théa <94928249+TheaCOLINOT@users.noreply.github.com>
2024-12-06 19:53:22 +0100

    fix init.sql

--- init.sql
+++ init.sql
@@ -5,4 +5,6 @@ CREATE TABLE `esgi`.`USERS` (
     `email` VARCHAR(255) NOT NULL , 
     `country` VARCHAR(50) NOT NULL , 
     `password` VARCHAR(255) NOT NULL , 
-RIMARY KEY (`id`)) ENGINE = InnoDB;
+    'createdat' TIMESTAMP NOT NULL ,
+    'updatedat' TIMESTAMP NOT NULL ,
+PRIMARY KEY (`id`)) ENGINE = InnoDB;
